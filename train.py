c5c04933a53dcf0320844689c130b9da2c31a767
jay5731 <niralipatel2400@gmail.com>
2026-08-20 10:38:17 +0530

    changed

--- train.py
+++ train.py
@@ -15,12 +15,12 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {DEVICE}")
 
 train_dir,test_dir=download_data()
-transform=transforms.Compose({
+transform=transforms.Compose([
     transforms.Resize((224,224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225])
-})
+])
 train_dataset = ImageFolder(train_dir, transform=transform)
 test_dataset  = ImageFolder(test_dir,  transform=transform)
 
